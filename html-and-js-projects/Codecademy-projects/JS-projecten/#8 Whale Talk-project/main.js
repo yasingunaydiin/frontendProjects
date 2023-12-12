c8c3fb76bc695f582f7588675d83358cb2463947
Yasin Gunaydin <81875281+yasingunaydiin@users.noreply.github.com>
2023-12-12 23:06:48 +0300

    new project

---
+++ html-and-js-projects/Codecademy-projects/JS-projecten/#8 Whale Talk-project/main.js
@@ -0,0 +1,14 @@
+const input = 'i like phrases'
+const vowels = ['a', 'e', 'i', 'o', 'u']
+const resultArray = []
+for (let inputIndex = 0; inputIndex < input.length; inputIndex++){
+    console.log(`inputIndex is ${inputIndex}.`)
+for (let vowelIndex = 0; vowelIndex < vowel.length; vowelIndex++) {
+    console.log(`vowelIndex is ${vowelIndex}.`)
+if (input[inputIndex] === vovels[vowelIndex]){
+    console.log(input[inputIndex])
+
+
+}
+}
+}
